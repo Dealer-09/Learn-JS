0f6533aea479ebf8b561c3d412a631dcd70b932c
Archis <archismanpaul09@outlook.com>
2024-09-28 20:16:44 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,30 @@
+alert('Archisman Pal');
+10+5
+2+2-5
+document.body.innerHTML='Archisman Pal';
+document.body.innerHTML=' ';
+1+1*3 //operator presedencde
+//4
+(1+1)*3
+//6
+20.95+77.99*2
+//36.93
+36.93*0.1
+(20.95+77.99*2)*0.1
+/*aacurate calculations (avoid float)
+rounding off*/
+console.log(Math.round((2095+799)*0.1)/100);
+fahrenheit=48;
+celsius=32;
+console.log("Temperature in celsius is "+Math.round((fahrenheit-32)*5/9));
+console.log("Temperature in fahrenheit is "+Math.round((celsius*9/5)+32));
+console.log('Archis'+'man');
+'hello'+28.05 //type conversion string+integer=string
+'I\'m Archisman'
+`I'm Archisman` //template String
+`Items(${1+1}) ${(2095+799)/100}`
+`Archisman 
+Pal`
+alert(`Total Cost (${1+1}): ${(599+295)/100}
+Thank you, come again !`);
+alert("I'm Archisman\n Pal"); // \n creates new line
